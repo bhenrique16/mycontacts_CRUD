141345c3a4fb6c92324b536a483e87193aac8c10
bhenrique16 <bh.henrique16@gmail.com>
2024-01-18 22:33:08 -0300

    Creating component select and input

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import { Container, Header, ListContainer, Card, InputSearchContainer } from "./styles";
-import arrow from "../../assets/images/icon/arrow.svg";
-import edit from "../../assets/images/icon/edit.svg";
-import trash from "../../assets/images/icon/trash.svg";
+import arrow from "../../assets/images/icons/arrow.svg";
+import edit from "../../assets/images/icons/edit.svg";
+import trash from "../../assets/images/icons/trash.svg";
 export default function Home() {
   return (
     <Container>
--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,3 +1,16 @@
+import Input from "../../components/Input";
+import PageHeader from "../../components/PageHeader";
+import Select from "../../components/Select";
+
 export default function NewContact() {
-  return 'NewContact';
+  return (
+
+    <>
+      <PageHeader title="Novo contato" />
+      <Input type="text" placeholder="nome" />
+      <Select>
+        <option value="123">Instagram </option>
+      </Select>
+    </>
+  )
 };
