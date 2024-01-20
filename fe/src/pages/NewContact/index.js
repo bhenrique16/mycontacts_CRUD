01ef8758e45b10291330b138c4271e076073d440
bhenrique16 <bh.henrique16@gmail.com>
2024-01-19 23:35:24 -0300

    Add Button component and update Select component in NewContact page

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,3 +1,4 @@
+import Button from "../../components/Button";
 import Input from "../../components/Input";
 import PageHeader from "../../components/PageHeader";
 import Select from "../../components/Select";
@@ -11,6 +12,12 @@ export default function NewContact() {
       <Select>
         <option value="123">Instagram </option>
       </Select>
+      <Button type="button" >
+        salvar
+      </Button>
+      <Button type="button" disabled >
+        salvar
+      </Button>
     </>
   )
 };
