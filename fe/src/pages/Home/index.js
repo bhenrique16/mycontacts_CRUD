36e60d95c22ddc8124a929c942e63374c19c5fda
bhenrique16 <bh.henrique16@gmail.com>
2024-03-05 22:05:45 -0300

    Display message when no contacts or registrations found

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom'
-import { useEffect, useState, useMemo, useCallback } from 'react';
-
+import { Link } from "react-router-dom";
+import { useEffect, useState, useMemo, useCallback } from "react";
+import magnifierQuestion from "../../assets/images/magnifier-question.svg";
 import {
   Container,
   Header,
@@ -8,105 +8,145 @@ import {
   Card,
   InputSearchContainer,
   ErrorContainer,
+  EmptyListContainer,
+  SearchNotFoundContainer,
 } from "./styles";
 
 import arrow from "../../assets/images/icons/arrow.svg";
 import edit from "../../assets/images/icons/edit.svg";
 import trash from "../../assets/images/icons/trash.svg";
-import sad from '../../assets/images/icons/sad.svg'
-import Loader from '../../components/Loader';
-import Button from '../../components/Button'
+import sad from "../../assets/images/icons/sad.svg";
+import Loader from "../../components/Loader";
+import Button from "../../components/Button";
+import emptyBox from "../../assets/images/empty-box.svg";
 
-
-import ContactsService from '../../services/ContactsService';
+import ContactsService from "../../services/ContactsService";
 
 export default function Home() {
-  const [contacts, setContacts] = useState([])
-  const [orderBy, setOrderBy] = useState('asc')
-  const [searchTerm, setSearchTerm] = useState('')
-  const [isLoading, setIsLoading] = useState(true)
-  const [hasError, setHasError] = useState(false)
+  const [contacts, setContacts] = useState([]);
+  const [orderBy, setOrderBy] = useState("asc");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
-  const filteredContacts = useMemo(() => {
-    return contacts.filter((contact) => (
-      contact.name.toLowerCase().startsWith(searchTerm.toLowerCase())
-    ))
-  }, [contacts, searchTerm])
+  const filteredContacts = useMemo(
+    () =>
+      contacts.filter((contact) =>
+        contact.name.toLowerCase().startsWith(searchTerm.toLowerCase())
+      ),
+    [contacts, searchTerm]
+  );
 
+  const loadContacts = useMemo(
+    () => async () => {
+      try {
+        setIsLoading(true);
 
-  const loadContacts = useCallback(async () => {
-    try {
-      setIsLoading(true)
-
-      const contactsList = await ContactsService.listContacts(orderBy)
-
-      setHasError(false)
-      setContacts(contactsList)
-    } catch (error) {
-      setHasError(true)
-    } finally {
-      setIsLoading(false)
-    }
-  }, [orderBy])
-
+        const contactsList = await ContactsService.listContacts(orderBy);
+        //const contactsList = [];
+        await ContactsService.listContacts(orderBy);
+        setHasError(false);
+        setContacts(contactsList);
+      } catch (error) {
+        setHasError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [orderBy]
+  );
 
   useEffect(() => {
-    loadContacts()
-  }, [loadContacts])
-
+    loadContacts();
+  }, [loadContacts]);
 
   function handleToggleOrderBy() {
-    setOrderBy(
-      (prevState) => (prevState === 'asc' ? 'desc' : 'asc'),
-    )
+    setOrderBy((prevState) => (prevState === "asc" ? "desc" : "asc"));
   }
 
   function handleChangeSearchTerm(event) {
-    setSearchTerm(event.target.value)
+    setSearchTerm(event.target.value);
   }
 
   function handleTryAgain() {
-    loadContacts()
+    loadContacts();
   }
 
   return (
     <Container>
       <Loader isLoading={isLoading} />
+
+      {contacts.length > 0 && (
+        <InputSearchContainer>
+          <input
+            value={searchTerm}
+            type="text"
+            placeholder="Pesquise pelo nome"
+            onChange={handleChangeSearchTerm}
+          />
+        </InputSearchContainer>
+      )}
       {/* <Modal danger /> */}
-      <InputSearchContainer>
-        <input
-          value={searchTerm}
-          type="text"
-          placeholder="Pesquise pelo nome"
-          onChange={handleChangeSearchTerm}
-        />
-      </InputSearchContainer>
-      <Header hasError={hasError}>
-        {!hasError && (
+
+      <Header
+        justifyContent={
+          hasError
+            ? "flex-end"
+            : contacts.length > 0
+            ? "space-between"
+            : "center"
+        }
+      >
+        {!!(!hasError && contacts.length) && (
           <strong>
             {filteredContacts.length}
-            {filteredContacts.length > 1 ? ' contatos' : ' contato'} </strong>
+            {filteredContacts.length > 1 ? " contatos" : " contato"}{" "}
+          </strong>
         )}
         <Link to="/new">Novo contato</Link>
       </Header>
       {hasError && (
         <ErrorContainer>
-          <img src={sad} alt='sad' />
-          <div className='details'>
+          <img src={sad} alt="sad" />
+          <div className="details">
             <strong>Ocorreu um erro ao obter os seus contatos!</strong>
-            <Button type="button" onClick={handleTryAgain}>Tentar novamente</Button>
+            <Button type="button" onClick={handleTryAgain}>
+              Tentar novamente
+            </Button>
           </div>
         </ErrorContainer>
       )}
+
       {!hasError && (
         <>
+          {contacts.length < 1 && !isLoading && (
+            <EmptyListContainer>
+              <img src={emptyBox} alt="empty box" />
+              <p>
+                Você ainda não tem nenhum contato cadastrado! Clique no botão
+                <strong>”Novo contato”</strong> à cima para cadastrar o seu
+                primeiro!
+              </p>
+            </EmptyListContainer>
+          )}
+
+          {contacts.length > 0 && filteredContacts.length < 1 && (
+            <SearchNotFoundContainer>
+              <img src={magnifierQuestion} alt="Magnifier Question" />
+
+              <span>
+                Nenhum resultado foi encontrado para{" "}
+                <strong>{searchTerm}</strong>.
+              </span>
+            </SearchNotFoundContainer>
+          )}
+
           {filteredContacts.length > 0 && (
             <ListHeader orderBy={orderBy}>
               <button type="button" onClick={handleToggleOrderBy}>
                 <span>Nome</span>
                 <img src={arrow} alt="Arrow" />
               </button>
-
             </ListHeader>
           )}
 
@@ -137,7 +177,5 @@ export default function Home() {
         </>
       )}
     </Container>
-  )
-};
-
-
+  );
+}
